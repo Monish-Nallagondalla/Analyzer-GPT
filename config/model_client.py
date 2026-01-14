a8eb8b1c62b1577763c47fe474a1a9c80a5cb8ac
Monish Nallagondalla <nsmonish@gmail.com>
2026-01-14 22:06:14 +0530

    feat: Add configuration and model client for OpenAI integration

---
+++ config/model_client.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from config.constants import OPENAI_API_KEY , MODEL 
+
+load_dotenv()
+
+api_key = os.getenv(OPENAI_API_KEY)
+
+
+def get_model_client():
+    if not api_key:
+        raise ValueError(f"Please set the {OPENAI_API_KEY} environment variable.")
+
+        model_client = OpenAIChatCompletionClient(
+            api_key=api_key,
+            model=MODEL,
+            text_mention_termination=TEXT_MENTION_TERMINATION,
+        )
+        return model_client
+    
